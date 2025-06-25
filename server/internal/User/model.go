0a17bb87919f90cd3c29951c7ccc8c6393bdb207
wuxxy <youwalkedoff@gmail.com>
2025-06-25 14:51:38 -0400

    Create server

---
+++ server/internal/User/model.go
@@ -0,0 +1,13 @@
+package User
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username   string `gorm:"unique"`
+	Email      string `gorm:"unique"`
+	Password   string
+	Permission int
+}
---
+++ server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/h_w", func(c echo.Context) error {
+		return c.String(http.StatusOK, ":)")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
